8938baa745bd19ccbf37e92d00c3cb8b747d9a53
programerlod <programerlod@icloud.com>
2021-05-24 11:32:08 -0500

    Finish Update

--- exp_jailbreak_new.js
+++ exp_jailbreak_new.js
@@ -156706,10 +156706,10 @@ var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 if (main_ret == 179 || main_ret == 0) {
 	localStorage.passcount = ++localStorage.passcount;window.passCounter.innerHTML=localStorage.passcount;
-  window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>New Exploit Loaded ✔</h1>";
+  window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Exploit nuevo (20-May by Sleirsgoevy) cargado exitosamente</h1>";
 } 
 else {
 	localStorage.failcount = ++localStorage.failcount;window.failCounter.innerHTML=localStorage.failcount;
-  window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Jailbreak failed! - Reboot your PS4 and try again.</h1>";
+  window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Liberacion Fallida! - reinicie su PS4 o intente con otra version del exploit.</h1>";
 }
 }
